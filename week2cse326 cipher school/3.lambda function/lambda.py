fd145465d0cd49789a661c0dcc9ffa5df36ff32d
Antra  Gupta <115134630+antra1947@users.noreply.github.com>
2022-12-09 22:29:11 +0530

    Add files via upload

---
+++ week2cse326 cipher school/3.lambda function/lambda.py
@@ -0,0 +1,27 @@
+a="antra"*2
+print(a)
+show = print
+show("antra")
+
+names = ["antra","gupta","soumya"]
+for name in enumerate(names):
+ print(names)
+
+ a = 5
+ b = 6
+ temp = a
+ a = b
+ b = temp
+
+ a = a+b
+ b = a-b
+ print(a,b)
+
+ a = [1,2]
+ b,c=a
+ print(b,c)
+
+ for i,name in enumerate(names):
+    print(i,"-",name)
+
+
